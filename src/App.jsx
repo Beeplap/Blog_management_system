bfbfe626d52d9afcf41c1ec06dac2fd9b0866e36
Beeplap <ghartibeeplap@gmail.com>
2025-05-08 06:59:40 +0545

    feat: added login and signup pages, test latest layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Management from './pages/Management';
 
-const App = () => {
+function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/management" element={<Management />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
